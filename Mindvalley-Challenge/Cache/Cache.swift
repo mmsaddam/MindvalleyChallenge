c7c37c7e265c0f12ac76854f248691a4124f167b
Muzahidul Islam <mujahid.islam@pathao.com>
2020-05-23 10:23:22 +0600

    Cache implementation done 	- UserDefault using as storage

---
+++ Mindvalley-Challenge/Cache/Cache.swift
@@ -0,0 +1,30 @@
+//
+//  Cache.swift
+//  Mindvalley-Challenge
+//
+//  Created by Muzahidul Islam on 22/5/20.
+//  Copyright © 2020 Muzahid. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol Cacheable {
+    func getData<T>(for key: String) -> T?
+    func saveData<T>(_ data: T, for key: String) throws -> Void
+}
+
+/**
+ UserDetault Cache
+ */
+final class Cache: Cacheable {
+    
+    func getData<T>(for key: String) -> T? {
+        UserDefaults.standard.value(forKey: key) as? T
+    }
+    
+    func saveData<T>(_ data: T, for key: String) throws -> Void{
+        UserDefaults.standard.set(data, forKey: key)
+    }
+    
+}
