9ec3e927f1721209dfef054eb07b3da99297f8ae
Muzahidul Islam <mujahid.islam@pathao.com>
2020-05-24 12:08:01 +0600

    ImageCache implemented

---
+++ Mindvalley-Challenge/ImageCache/UIImageView+Cache.swift
@@ -0,0 +1,21 @@
+//
+//  UIImageView+Cache.swift
+//  Mindvalley-Challenge
+//
+//  Created by Muzahidul Islam on 24/5/20.
+//  Copyright © 2020 Muzahid. All rights reserved.
+//
+
+import UIKit
+
+extension UIImageView {
+    func loadImage(_ url: URL, placeHolder: UIImage?) {
+        ImageCache.publicCache.load(url: url as NSURL) { [weak self] (remoteImage) in
+            if remoteImage != nil {
+                self?.image = remoteImage
+            } else if let placeHolder = placeHolder {
+                self?.image = placeHolder
+            }
+        }
+    }
+}
