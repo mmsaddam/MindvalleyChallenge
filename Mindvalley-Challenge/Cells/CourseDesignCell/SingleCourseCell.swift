5e2f415da90e82ab0009e0559fb2189d33386200
Muzahidul Islam <mujahid.islam@pathao.com>
2020-05-24 11:52:15 +0600

    Collection view cell for course design added

---
+++ Mindvalley-Challenge/Cells/CourseDesignCell/SingleCourseCell.swift
@@ -0,0 +1,50 @@
+//
+//  SingleCourseCell.swift
+//  Mindvalley-Challenge
+//
+//  Created by Muzahidul Islam on 23/5/20.
+//  Copyright © 2020 Muzahid. All rights reserved.
+//
+
+import UIKit
+
+struct SingleCourseCellViewModel {
+    let state: State<Media>
+}
+
+
+final class SingleCourseCell: UICollectionViewCell {
+    @IBOutlet weak var coverPhoto: UIImageView!
+    @IBOutlet weak var eposodeName: UILabel!
+    @IBOutlet weak var channelName: UILabel!
+    @IBOutlet weak var loadingView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        coverPhoto.layer.cornerRadius = 4.0
+    }
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        coverPhoto.image = nil
+    }
+
+}
+
+extension SingleCourseCell: CellConfigurable {
+    typealias ModelType = SingleCourseCellViewModel
+    
+    func configure(model: SingleCourseCellViewModel) {
+        if case let .loaded(episode) = model.state {
+            loadingView.isHidden = true
+            sendSubviewToBack(loadingView)
+            if let url = episode.coverUrl {
+                coverPhoto.loadImage(url, placeHolder: nil)
+            }
+            eposodeName.text = episode.title
+            channelName.text = episode.channelTitle
+        } else {
+            bringSubviewToFront(loadingView)
+            loadingView.isHidden = false
+        }
+    }
+}
